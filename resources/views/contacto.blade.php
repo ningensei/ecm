cd9e6eb1ff8c9f1527c2127d765fe21b59e53776
Pablo Tomeo <pablo.tomeo@gmail.com>
2017-09-03 23:35:26 -0300

    lenguaje form

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -6,17 +6,17 @@
 	
     <div class="container">
         
-        <div class="row" style="position: relative;">
+        <div class="row">
         
-            <iframe src="https://docs.google.com/forms/d/1epYiEEgiMWxSYAp69t6_MMchtuv-hi4E8R5XJBtCNw0/viewform?embedded=true" width="100%" height="1890px" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>
+            <iframe src="https://docs.google.com/forms/d/1epYiEEgiMWxSYAp69t6_MMchtuv-hi4E8R5XJBtCNw0/viewform?embedded=true&hl=es" width="100%" height="1940px" frameborder="0" marginheight="0" marginwidth="0">Cargando...</iframe>
 
-            <div class="coverform" style="
+            <!-- <div class="coverform" style="
                 position: absolute;
                 bottom: 0;
                 width: 100%;
                 height: 117px;
                 background: #f4f4f4;
-            "></div>
+            "></div> -->
 
         </div><!-- ./row -->
 
